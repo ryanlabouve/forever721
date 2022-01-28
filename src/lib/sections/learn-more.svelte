11dea2fb9e60c5e3ac76732969c4178609aa523f
Ryan LaBouve <imryanlabouve@gmail.com>
2022-01-27 23:31:34 -0600

    Checkpoint

---
+++ src/lib/sections/learn-more.svelte
@@ -0,0 +1,128 @@
+<script lang="ts">
+	let metaData = [
+		{
+			label: 'Good',
+			color: 'green',
+			illo: '',
+			heading: 'All data on chain',
+			example: '',
+			description: 'All of the metadata '
+		},
+		{
+			label: 'Good',
+			color: 'green',
+			illo: '',
+			heading: 'Data stored immutably',
+			example: '',
+			description: 'IPFS'
+		},
+		{
+			label: 'Good',
+			color: 'green',
+			illo: '',
+			heading: 'Metadata off-chain',
+			example: '',
+			description: 'Centralized servers'
+		}
+	];
+
+	let storage = [
+		{
+			label: 'Good',
+			color: 'green',
+			illo: '',
+			heading: 'All data on chain',
+			example: '',
+			description: 'All of the metadata '
+		},
+		{
+			label: 'Good',
+			color: 'green',
+			illo: '',
+			heading: 'Data stored immutably',
+			example: '',
+			description: 'IPFS'
+		},
+		{
+			label: 'Good',
+			color: 'green',
+			illo: '',
+			heading: 'Metadata off-chain',
+			example: '',
+			description: 'Centralized servers'
+		}
+	];
+</script>
+
+<div class="bg-zinc-200  py-8">
+	<img class="xl:float-right  m-auto " style="max-width: 800px;" src="/images/protect.png" />
+	<div class="max-w-4xl m-auto px-3 my-8 prose">
+		<div class="heading text-5xl mb-3">Learn More</div>
+		<div>
+			<div class="mb-4">
+				<div class="text-xl">Why did we make this?</div>
+				<div>
+					<div>↳ Help people new to NFTs understand what they are buying</div>
+					<div>↳ Provide an accessible technical view of your NFT collection</div>
+					<div>↳ Provide an easy backup solution for mementos and redundancy</div>
+				</div>
+			</div>
+			<blockquote>
+				When you are buying an NFT, the value is in the metadata. Ensuring the meatadata is stored
+				immutably and durably is key
+			</blockquote>
+			<div>More about grading</div>
+			<div class="grid grid-cols-3 gap-3">
+				<div>Holder</div>
+			</div>
+			<div>
+				<div class="text-xl">About grading</div>
+				<div>
+					Metadata https://opensea.io/blog/guides/non-fungible-tokens/#On-chain_vs_off-chain
+				</div>
+				<div>
+					Storage https://opensea.io/blog/guides/non-fungible-tokens/#Off-chain_storage_solutions
+				</div>
+			</div>
+
+			<h3>References</h3>
+			<div>
+				<div>
+					<a href="https://newsletter.banklesshq.com/p/the-first-100m-in-nfts">
+						https://newsletter.banklesshq.com/p/the-first-100m-in-nfts
+					</a>
+				</div>
+				<div>
+					<a href="https://future.a16z.com/nfts-thousand-true-fans/"
+						>https://future.a16z.com/nfts-thousand-true-fans/</a
+					>
+				</div>
+				<div>
+					<a href="https://andrewsteinwold.substack.com/p/-quick-overview-of-the-nft-ecosystem"
+						>https://andrewsteinwold.substack.com/p/-quick-overview-of-the-nft-ecosystem</a
+					>
+				</div>
+				<div>
+					<a
+						href="https://linda.mirror.xyz/df649d61efb92c910464a4e74ae213c4cab150b9cbcc4b7fb6090fc77881a95d"
+					>
+						https://linda.mirror.xyz/df649d61efb92c910464a4e74ae213c4cab150b9cbcc4b7fb6090fc77881a95d
+					</a>
+				</div>
+				<div>
+					<a href="https://justincone.com/posts/nft-skeptics-guide/">
+						https://justincone.com/posts/nft-skeptics-guide/
+					</a>
+				</div>
+				<div>
+					<a href="https://opensea.io/blog/guides/non-fungible-tokens/"
+						>https://opensea.io/blog/guides/non-fungible-tokens/</a
+					>
+				</div>
+				<div>
+					<a href="https://tofunft.com/tools/validator">https://tofunft.com/tools/validator</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
